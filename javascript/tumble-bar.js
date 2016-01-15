4f7b975a4f6367f239e9d24f35c33b33139090fb
Tumble <tumblegamer@tumblenet.tk>
2016-01-15 22:12:44 +0000

    Update tumble-bar.js

--- javascript/tumble-bar.js
+++ javascript/tumble-bar.js
@@ -1,5 +1,3 @@
-$(document).ready(function(){ 
-  $.get("http://search.tumblenet.tk/tumble-bar.html", function(data) {
-    $("#TumbleBar").html(data);
-  });
-});
+$.get("http://search.tumblenet.tk/tumble-bar.html", function(data) {
+   $("#TumbleBar").html(data);
+ });
