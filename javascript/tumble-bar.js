c7de685bb3161e75113c35c322a5d79288350afa
Tumble <tumblegamer@tumblenet.tk>
2016-01-17 10:57:52 +0000

    Update tumble-bar.js

--- javascript/tumble-bar.js
+++ javascript/tumble-bar.js
@@ -1,4 +1,4 @@
 //$("#tumble").load("tumble-bar.html", function () {/* callback if needed */});
-$(document).ready(function(){
+$(document).ready(
         $("#tumble").load("http://search.tumblenet.tk/tumble-bar.html");
-});
+);
