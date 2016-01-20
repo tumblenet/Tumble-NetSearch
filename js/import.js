9dab786c325722ddedb3b1256ce072abdf387a4e
Tumble <tumblegamer@tumblenet.tk>
2016-01-20 19:03:29 +0000

    Update import.js

--- js/import.js
+++ js/import.js
@@ -1,5 +1,5 @@
 //$(document).ready(function() {
-    var url = $(".import").getAttribute("title');;
+    var url = $(".import").getAttribute("title');
     alert(url);
     //$(".import[title=" + url + "]").load();
 //});
