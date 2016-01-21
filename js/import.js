40dd4f6feabf85ebb61c4fc85e579b62c3941e00
Tumble <tumblegamer@tumblenet.tk>
2016-01-21 17:18:03 +0000

    Update import.js

--- js/import.js
+++ js/import.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
    if (document.querySelectorAll)
         var clsElements = document.querySelectorAll(".import");
         var url = $(clsElements).attr("href");
-        //var id = $(clsElements).attr("id");
+        var id = $(clsElements).attr("id");
+        alert(id)
         $(clsElements).load(url);
         else
         // loop through all elements instead
