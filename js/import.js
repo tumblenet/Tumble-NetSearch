5a6805ec7837acefb418c00cb6c6cfc89d2acc43
Tumble <tumblegamer@tumblenet.tk>
2016-01-21 18:49:04 +0000

    Update import.js

--- js/import.js
+++ js/import.js
@@ -1,11 +1,9 @@
-$(function() {
-    /*var id = $(".import").attr("id");
-    var url = $(".import").attr("href");
-    $("#" + id + " ,").load(.attr("href"));*/
-    
-    loadURL($(".import").attr("id"), $(".import").attr("href"));
-    
-    function loadURL(id, url) {
-        $(".import, #" + id).load(url));
-    }
-    });
+/*var id = $(".import").attr("id");
+var url = $(".import").attr("href");
+$("#" + id + " ,").load(.attr("href"));*/
+
+loadURL($(".import").attr("id"), $(".import").attr("href"));
+
+function loadURL(id, url) {
+    $(".import, #" + id).load(url));
+}
