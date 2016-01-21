acdd75f2bb6f4c86e86aabae32bfc134554d90e5
Tumble <tumblegamer@tumblenet.tk>
2016-01-21 17:34:14 +0000

    Update import.js

--- js/import.js
+++ js/import.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
     
-   if (document.querySelectorAll) {
-        var clsElements = document.querySelectorAll(".import");
-        var url = $(clsElements).attr("href");
-        var id = $(clsElements).attr("id");
-        alert(id);
-        $(clsElements).load(url);
-       
-   } else {
-        // loop through all elements instead
-    }
+    //var id = $(.import).attr("id");
+    //var url = $(.import).attr("href");
+   
+    //alert(id);
+    $(.import).load($(.import).attr("href"));
