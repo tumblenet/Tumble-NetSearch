76b0eb18a088dea16c330b1cab9d6cf3869b3e8a
Tumble <tumblegamer@tumblenet.tk>
2016-01-21 19:05:37 +0000

    Update import.js

--- js/import.js
+++ js/import.js
@@ -2,10 +2,6 @@
 var url = $(".import").attr("href");
 $("#" + id + " ,").load(.attr("href"));*/
 
-$(function() {
-    $(.import).attr("id").getURL(id);
-});
-
 $( "#import" ).load(function() {
     var id = $(this).attr("id");
     alert(id;)
