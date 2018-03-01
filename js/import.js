52a0a9a50d0e8a845479cb2a4ed4ddb9fe2d29e6
Tumble <tumblegamer@gmail.com>
2018-03-01 19:40:23 +0000

    Under Construction

--- js/import.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-      $( ".import" ).each(function( i ) {
-            console.log('i: ' + i);
-            var url = $( this ).attr( "title" );
-             console.log('url: ' + url);
-            $( this ).load( url );
-      });
-            
-  });
